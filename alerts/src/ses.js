f21c668e96e6f638c28f555b12048f43019f6686
DivyanshuKaushik <divyanshukaushik44@gmail.com>
2022-08-19 14:26:49 +0530

    hidden secrets

--- alerts/src/ses.js
+++ alerts/src/ses.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-    accessKeyId:"AKIASTVP7WBB27JZOY7K",
-    secretAccessKey:"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",
-    region: 'ap-south-1'
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
 });
 const ses = new AWS.SES();
 
